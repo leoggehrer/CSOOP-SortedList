8d29cabeb504dfb24f29119c23bd465bbff77453
leoggehrer <g.gehrer@htl-leonding.ac.at>
2022-11-30 09:28:35 +0100

    Add project files.

---
+++ SortedList.ConApp/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace SortedList.ConApp
+{
+    internal class Program
+    {
+        static Random Random = new Random(DateTime.Now.Millisecond);
+        static void Main(string[] args)
+        {
+            SortedList list = new SortedList();
+
+            Console.WriteLine("Demo Sorted-List!");
+
+            // Insert 10 numbers
+            for (int i = 0; i < 10; i++)
+            {
+                list.Insert(Random.Next(1, 100));
+            }
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.Write($" {list.GetAt(i)}");
+            }
+        }
+    }
+}
---
+++ SortedList.ConApp/SortedList.cs
@@ -0,0 +1,56 @@
+﻿namespace SortedList.ConApp
+{
+    public class SortedList
+    {
+        private class Element
+        {
+            public int Data { get; set; }
+            public Element? Next { get; set; }
+            public Element(int data, Element? next)
+            {
+                Data = data;
+                Next = next;
+            }
+        }
+
+        /// <summary>
+        /// Calculates the number of elements in the list.
+        /// </summary>
+        public int Count
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        /// <summary>
+        /// Inserts the value into the list in ascending order.
+        /// </summary>
+        /// <param name="value">The value to insert.</param>
+        /// <exception cref="NotImplementedException"></exception>
+        public void Insert(int value)
+        {
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// Removes the position [0..Count-1], otherwise an IndexOutOfRange is thrown.
+        /// </summary>
+        /// <param name="position">The position [0..Count-1] in the list.</param>
+        /// <exception cref="NotImplementedException"></exception>
+        public void Remove(int position)
+        {
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// Returns the value at the lite position to the caller.
+        /// </summary>
+        /// <param name="position">The position [0..Count-1] in the list.</param>
+        /// <returns>The value at the position, otherwise an IndexOutOfRange is thrown.</returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public int GetAt(int position)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
